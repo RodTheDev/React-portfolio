9bc7c2e29771043c83f7e96d031ebba3058e5113
rbarocio <rodrigobarocio@gmail.com>
2019-01-08 11:30:37 -0800

    modified contact.js added <ul>

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,8 +7,8 @@ class Contact extends Component {
       <h1>Contact Page.</h1>
       <p>This is a paragraph.</p>
       <ul>
-        <li>List item 1</li>
-        <li>List item 2</li>
+        <li>List item 1.</li>
+        <li>List item 2.</li>
       </ul>
     </div>
   )
